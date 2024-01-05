290278cece58b74b83483e1a51c5ca612ce49bb2
andyfullstack <andriikoliadenko@gmail.com>
2024-01-05 11:24:57 +0200

    up1111d

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,6 +8,7 @@ export const App = () => {
         alignItems: 'center',
         fontSize: 40,
         color: '#723b3b',
+        backgroundColor: '#111',
       }}
     >
       React homework template
